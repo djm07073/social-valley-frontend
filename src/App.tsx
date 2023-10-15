f7713412b06eb74467a9d222059391662cf53d8a
Riudiu <lacvert13@gmail.com>
2023-10-15 23:35:38 +0900

    Initial commit ⛰️

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+/** @jsxImportSource @emotion/react */
+import { useState } from "react";
+import { css } from "@emotion/react";
+
+export default function App() {
+  return <div css={{ padding: "0px 20px" }}>Social Valley ⛰️</div>;
+}
