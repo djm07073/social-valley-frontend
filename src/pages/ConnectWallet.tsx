021ea63c91e486dea4a2bcde16afa28804f58d91
Riudiu <lacvert13@gmail.com>
2023-10-17 17:38:15 +0900

    UI : not following page 완성

--- src/pages/ConnectWallet.tsx
+++ src/pages/ConnectWallet.tsx
@@ -16,40 +16,38 @@ export default function ConnectWallet() {
         width={102}
         alt="valley"
       />
-      <div>
-        <div
-          css={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginTop: 45,
-            marginBottom: 14,
-          }}
-        >
-          <img
-            src={process.env.PUBLIC_URL + "/assets/lg_polygon.png"}
-            width={14}
-            alt="polygon"
-          />
-          <div css={{ marginLeft: 6, fontSize: 12, fontWeight: 300 }}>
-            Polygon
-          </div>
+      <div
+        css={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 45,
+          marginBottom: 14,
+        }}
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/assets/lg_polygon.png"}
+          width={14}
+          alt="polygon"
+        />
+        <div css={{ marginLeft: 6, fontSize: 12, fontWeight: 300 }}>
+          Polygon
         </div>
-        <div
-          css={{
-            width: 236,
-            height: 42,
-            textAlign: "center",
-            padding: "10px 0px",
-            backgroundColor: "#338A46",
-            fontSize: 14,
-            color: "white",
-            cursor: "pointer",
-            fontWeight: 300,
-          }}
-        >
-          Connect Wallet
-        </div>
+      </div>
+      <div
+        css={{
+          width: 236,
+          height: 42,
+          textAlign: "center",
+          padding: "10px 0px",
+          backgroundColor: "#338A46",
+          fontSize: 14,
+          color: "white",
+          cursor: "pointer",
+          fontWeight: 300,
+        }}
+      >
+        Connect Wallet
       </div>
     </div>
   );
--- src/pages/NotFollowing.tsx
+++ src/pages/NotFollowing.tsx
@@ -1,5 +1,71 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import { useState } from "react";
 
 export default function NotFollowing() {
-  return <div>NotFollowing</div>;
+  const [groupId, setGroupId] = useState("{Group id}");
+  const [vely, setVely] = useState(0);
+
+  return (
+    <div
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <img
+        src={process.env.PUBLIC_URL + "/assets/img_valley_2.png"}
+        width={78}
+        alt="valley_2"
+        css={{ marginBottom: 15 }}
+      />
+      <div css={{ fontSize: 12, fontWeight: 400, marginBottom: 3 }}>
+        {groupId}
+      </div>
+      <div
+        css={{
+          fontSize: 18,
+          fontWeight: 500,
+          marginBottom: 43,
+          color: "#338A46",
+        }}
+      >
+        {vely} vely
+      </div>
+      <div
+        css={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: 14,
+        }}
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/assets/lg_web3_storage.png"}
+          width={14}
+          alt="web3_storage"
+        />
+        <div css={{ marginLeft: 6, fontSize: 12, fontWeight: 300 }}>
+          Web3 storage
+        </div>
+      </div>
+      <div
+        css={{
+          width: 236,
+          height: 42,
+          textAlign: "center",
+          padding: "10px 0px",
+          backgroundColor: "#338A46",
+          fontSize: 14,
+          color: "white",
+          cursor: "pointer",
+          fontWeight: 300,
+        }}
+      >
+        Check this user
+      </div>
+    </div>
+  );
 }
